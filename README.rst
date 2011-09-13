cff1471c77d6e3f32752194066d624215394299c
Jens W. Klein <jens@bluedynamics.com>
2011-09-13 11:27:10 +0200

    initial structure

---
+++ README.rst
@@ -0,0 +1,39 @@
+Integartion of Soundcloud in Plone
+==================================
+
+WRITE ME
+
+Installation
+============
+
+Just depend in your buildout on the egg ``collective.soundcloud``. ZCML is
+loaded automagically with z3c.autoinclude.
+
+Install it as an addon in Plone control-panel or portal_setup.
+
+This package is written for Plone 4.1 or later.
+
+Source Code and Contributions
+=============================
+
+If you want to help with the development (improvement, update, bug-fixing, ...)
+of ``collective.soundcloud`` this is a great idea!
+
+The code is located in the
+`github collective <https://github.com/collective/collective.soundcloud>`_.
+
+You can clone it or `get access to the github-collective
+<http://collective.github.com/>`_ and work directly on the project.
+
+Maintainers are Jens Klein, Peter Holzer and the BlueDynamics Alliance 
+developer team. We appreciate any contribution and if a release is needed 
+to be done on pypi, please just contact one of us
+`dev@bluedynamics dot com <mailto:dev@bluedynamics.com>`_
+
+Contributors
+============
+
+- Jens W. Klein <jens@bluedynamics.com>
+
+- Peter Holzer <hpeter@agitator.com>
+
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+shortdesc = 'Soundcloud integration for Plone'
+longdesc =  open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()  
+longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()  
+longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()  
+
+setup(name='collective.soundcloud',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python', 
+            'Topic :: Software Development',
+            "Framework :: Plone",
+      ],
+      keywords='',
+      author='Jens Klein    ',
+      author_email='jens@bluedynamics.com',
+      url=u'http://github.com/collective/collective.soundcloud',
+      license='GPLv2',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+            'setuptools',
+            'Plone',
+            'yafowil.zope2',
+            'yafowil.yaml',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
