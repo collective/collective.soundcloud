0defdf0d9061be801606c0a1ebcd3d940172bc44
Jens W. Klein <jens@bluedynamics.com>
2012-01-13 09:42:38 +0100

    more form, upload, file with opts

--- src/collective/soundcloud/addedit/form.py
+++ src/collective/soundcloud/addedit/form.py
@@ -1,4 +1,5 @@
 import copy
+from restkit import RequestError
 from zope.i18nmessageid import MessageFactory
 from zExceptions import Unauthorized
 from Products.Five import BrowserView
@@ -12,13 +13,19 @@ from collective.soundcloud.utils import get_soundcloud_api
 _ = MessageFactory('collective.soundcloud')
 
 EDIT, ADD = 1, 2
+FILEMARKER = object()
 DEFAULTS = {
     'title': UNSET,
     'description': UNSET,
     'asset_data': UNSET,
+    'track_type': UNSET,
+    'genre': UNSET,
+    'tag_list': "",
+    'license': 'all-rights-reserved',
 }  
 
 VOCAB_TRACK_TYPES = [
+    "",
     "original",
     "remix",
     "live",
@@ -45,14 +52,20 @@ VOCAB_LICENSES = [
     "cc-by-nc-sa",
 ]
 
+VOCAB_FILEOPTS = [
+    ('keep', u'Keep Existing file'),
+    ('replace', u'Replace existing file'),
+]
+
 class SoundcloudAddEdit(BrowserView):    
     
     def form(self):
         self.mode = ADD        
-        self.trackdata = copy.deepcopy(DEFAULTS)
+        self.trackdata = dict(DEFAULTS)
         if ISoundcloudItem.providedBy(self.context):
             self.mode = EDIT
             self.trackdata = copy.deepcopy(self.context.trackdata)
+            self.trackdata['asset_data'] = FILEMARKER
         form = parse_from_YAML('collective.soundcloud.addedit:form.yaml',
                                self,  _)        
         controller = Controller(form, self.request)
@@ -61,30 +74,47 @@ class SoundcloudAddEdit(BrowserView):
         self.request.RESPONSE.redirect(controller.next)
             
     def next(self, request):
-        if self.mode == ADD:
-            return '%s/++soundcloud++%s' % (self.context.absolute_url(), 
-                                            self.soundcloud_id)
-        else:
-            return self.context.absolute_url()
+        return '%s/++soundcloud++%s' % (self.context.absolute_url(), 
+                                        self.soundcloud_id)
 
     @property
     def action(self):
-        return '%s/@@soundcloud_modifier' % self.context.absolute_url() 
+        if self.mode == 'EDIT':
+            url = '%s/++soundcloud++%s' % (self.context.absolute_url(), 
+                                           self.soundcloud_id)
+        else:
+            url = self.context.absolute_url() 
+        return '%s/@@soundcloud_modifier' % url
     
     def save(self, widget, data):
         if self.request.method != 'POST':
             raise Unauthorized('POST only')
         # upload here
         upload_track_data = dict()
-        upload_track_data['title'] = data['title'].extracted
-        upload_track_data['description'] = data['description'].extracted 
-        upload_track_data['asset_data'] = data['soundfile'].extracted['file']
+        for key in DEFAULTS:
+            if data[key].extracted is UNSET:
+                continue
+            if key == 'asset_data':
+                if data[key].extracted is FILEMARKER:
+                    continue
+                else:
+                    upload_track_data[key] = data[key].extracted['file']
+            else:
+                upload_track_data[key] = data[key].extracted
         sc = get_soundcloud_api()
-        tracks = sc.tracks()        
-        trackdata = tracks(upload_track_data)
+        if self.mode == EDIT:
+            tracks = sc.tracks(self.context.trackdata['id']) 
+        else:
+            tracks = sc.tracks()     
+        #import pdb;pdb.set_trace()
+        try:
+            trackdata = tracks(upload_track_data)
+        except RequestError:
+            # TODO Error handling
+            raise
         if self.mode == EDIT:
             self.context.trackdata = trackdata
-        self.soundcloud_id = trackdata
+        self.soundcloud_id = trackdata['id']
         
     @property
     def vocab_track_types(self):        
@@ -93,3 +123,8 @@ class SoundcloudAddEdit(BrowserView):
     @property
     def vocab_licenses(self):
         return VOCAB_LICENSES
+       
+    @property
+    def vocab_fileopts(self):
+        return VOCAB_FILEOPTS
+    
