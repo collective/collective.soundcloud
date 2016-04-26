57fdeaa16e3098666a46591600ccf8304864eeb0
Jens W. Klein <jk@kleinundpartner.at>
2016-04-26 10:05:30 +0200

    taskqueue init

---
+++ src/collective/soundcloud/taskqueue/addedit.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+SC_FIELDS = {
+    '': '',
+}
+
+
+def queue_upload(obj, descriptions=None):
+    """if descriptions is None, this is an add
+    """
+
+    if descriptions is None:
+        # add, all fields are changed
+        pass
+    else:
+        if len(descriptions) == 0:
+            return
+        # edit, look which relevant fields are changed
+        pass
+
+
+def soundcloud_add(obj, event):
+    queue_upload(obj, mode='add')
+
+
+def soundcloud_edit(obj, event):
+    queue_upload(obj, event.descriptions=[])
---
+++ src/collective/soundcloud/taskqueue/uploader.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from Products.Five import BrowserView
+from collective.soundcloud.events import SoundcloudCreatedEvent
+from collective.soundcloud.events import SoundcloudModifiedEvent
+from collective.soundcloud.utils import get_soundcloud_api
+from zope.event import notify
+
+
+class SoundcloudUploaderView(BrowserView):
+
+    def async_upload_handler(self):
+        sc = get_soundcloud_api()
+        if self.soundcloud_id:
+            # we're are in soundcloud edit mode,
+            # iow this was already uploaded to soundcloud
+            tracks = sc.tracks(self.soundcloud_id)
+            if tfname:
+                tracks(delete=True)
+                tracks = sc.tracks()
+        else:
+            # no soundcloud upload so far.
+            tracks = sc.tracks()
+
+        if tfname:
+            tdir = os.path.dirname(tfname)
+            try:
+                tf = open(tfname, 'rb')
+                upload_track_data['asset_data'] = tf
+                upload_track_data = upload(tracks, upload_track_data)
+            finally:
+                tf.close()
+                shutil.rmtree(tdir)
+        else:
+            upload_track_data = upload(tracks, upload_track_data)
+        setattr(context, 'trackdata', upload_track_data)
+        setattr(context, 'soundcloud_id', upload_track_data['id'])
+        self.context._p_changed = 1
+        if mode == 'edit':
+            notify(SoundcloudModifiedEvent(context))
+        else:
+            notify(SoundcloudCreatedEvent(context))
+
+
+    def __call__(self):
+        pass
