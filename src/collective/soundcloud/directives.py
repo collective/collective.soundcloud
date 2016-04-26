51eb98560bfe615e81818877ba9fc2de50a8a28f
Jens W. Klein <jk@kleinundpartner.at>
2016-04-26 13:47:58 +0200

    more uploader logic

--- src/collective/soundcloud/directives.py
+++ src/collective/soundcloud/directives.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from plone.supermodel.directives import MetadataListDirective
+from plone.supermodel.utils import mergedTaggedValueList
+from plone.behavior.interfaces import IBehaviorAssignable
 
 SOUNDCLOUD_KEY = 'collective.soundcloud.metainfo.fields'
 SOUNDFILE_KEY = 'collective.soundcloud.metainfo.file'
@@ -7,18 +9,18 @@ SOUNDFILE_KEY = 'collective.soundcloud.metainfo.file'
 
 class SoundcloudMeta(object):
 
-    def __init__(self, fields=[], soundfile=None):
+    def __init__(self, accessors=[], soundfile=None):
         self.soundfile = soundfile
-        self.fields = fields
+        self.accessors = accessors
 
 
 class soundcloud(MetadataListDirective):
-    """Directive used to create fieldsets
+    """Directive used find soudncloud data accessors
     """
     key = SOUNDCLOUD_KEY
 
     def factory(self, *args):
-        metainfo = SoundcloudMeta(fields=args)
+        metainfo = SoundcloudMeta(accessors=args)
         return [metainfo]
 
 
@@ -28,5 +30,29 @@ class soundfile(MetadataListDirective):
     key = SOUNDFILE_KEY
 
     def factory(self, filefield):
-        metainfo = SoundcloudMeta(soundfile=soundfile)
+        metainfo = SoundcloudMeta(soundfile=filefield)
         return [metainfo]
+
+
+def get_soundfile_field(context):
+    assignable = IBehaviorAssignable(context, None)
+    if assignable is None:
+        return
+    for behavior_registration in assignable.enumerateBehaviors():
+        schema = behavior_registration.interface
+        for tgv in mergedTaggedValueList(schema, SOUNDFILE_KEY):
+            if tgv.soundfile:
+                return tgv.soundfile
+
+
+def get_soundcloud_accessors(context):
+    accessors = []
+    assignable = IBehaviorAssignable(context, None)
+    if assignable is None:
+        return accessors
+    for behavior_registration in assignable.enumerateBehaviors():
+        schema = behavior_registration.interface
+        for tgv in mergedTaggedValueList(schema, SOUNDCLOUD_KEY):
+            for accessor in tgv.accessors:
+                accessors.append = behavior_registration.interface, accessor
+    return accessors
--- src/collective/soundcloud/taskqueue/uploader.py
+++ src/collective/soundcloud/taskqueue/uploader.py
@@ -1,58 +1,67 @@
 # -*- coding: utf-8 -*-
-from Products.Five import BrowserView
+from collective.soundcloud.directives import get_soundcloud_accessors
+from collective.soundcloud.directives import get_soundfile_field
 from collective.soundcloud.events import SoundcloudCreatedEvent
 from collective.soundcloud.events import SoundcloudModifiedEvent
 from collective.soundcloud.utils import get_soundcloud_api
+from Products.Five import BrowserView
+from restkit import RequestError
 from zope.event import notify
-from plone.behavior.interfaces import IBehaviorAssignable
+
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class SoundcloudUploaderView(BrowserView):
 
-    def async_upload_handler(self):
+    def upload(self, tracks, upload_track_data):
+        try:
+            return tracks(upload_track_data)
+        except RequestError:
+            logger.exception('Can not modify at/upload to Soundcloud!')
+            raise
+
+    def async_upload_handler(self, changed_fields):
         sc = get_soundcloud_api()
+        filefield = get_soundfile_field(self.context)
+        accessors = get_soundcloud_accessors(self.context)
+        import pdb; pdb.set_trace()
 
-        # fetch fields and blob
-        assignable = IBehaviorAssignable(self, None)
-        if assignable is not None:
-            for behavior_registration in assignable.enumerateBehaviors():
-                pass
-
-        if self.soundcloud_id:
+        file_has_changed = filefield in changed_fields
+        mode = 'edit' if self.context.soundcloud_id else 'add'
+        # fetch blob
+        if self.context.soundcloud_id:
             # we're are in soundcloud edit mode,
             # iow this was already uploaded to soundcloud
             tracks = sc.tracks(self.soundcloud_id)
-            if tfname:
+            if file_has_changed:
+                # delete what we have at soundcloud and create new entry
                 tracks(delete=True)
                 tracks = sc.tracks()
         else:
             # no soundcloud upload so far.
             tracks = sc.tracks()
 
-        assignable = IBehaviorAssignable(self, None)
-        if assignable is not None:
-            for behavior_registration in assignable.enumerateBehaviors():
-                pass
+        upload_track_data = {}
+        for iface, accessor in accessors:
+            adapter = iface(self.context)
+            upload_track_data[accessor] = getattr(adapter, accessor)
 
-        if tfname:
-            tdir = os.path.dirname(tfname)
-            try:
-                tf = open(tfname, 'rb')
-                upload_track_data['asset_data'] = tf
-                upload_track_data = upload(tracks, upload_track_data)
-            finally:
-                tf.close()
-                shutil.rmtree(tdir)
-        else:
-            upload_track_data = upload(tracks, upload_track_data)
-        setattr(context, 'trackdata', upload_track_data)
-        setattr(context, 'soundcloud_id', upload_track_data['id'])
+        if file_has_changed:
+            # fetch blob as open file-like object
+            file_handle = None  # XXX
+            upload_track_data['asset_data'] = file_handle
+        upload_track_data = self.upload(tracks, upload_track_data)
+        setattr(self.context, 'trackdata', upload_track_data)
+        setattr(self.context, 'soundcloud_id', upload_track_data['id'])
         self.context._p_changed = 1
         if mode == 'edit':
-            notify(SoundcloudModifiedEvent(context))
+            notify(SoundcloudModifiedEvent(self.context))
         else:
-            notify(SoundcloudCreatedEvent(context))
-
+            notify(SoundcloudCreatedEvent(self.context))
 
     def __call__(self):
-        pass
+        self.async_upload_handler()
+        return 'foo'
