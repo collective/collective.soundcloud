7d42eba54b44f4f220f0fda5006c446009c2f95b
Jens W. Klein <jk@kleinundpartner.at>
2016-05-09 16:49:35 +0200

    increase debugability

--- src/collective/soundcloud/taskqueue/uploader.py
+++ src/collective/soundcloud/taskqueue/uploader.py
@@ -26,25 +26,9 @@ class SoundcloudUploaderView(BrowserView):
             logger.exception('Can not modify at/upload to Soundcloud!')
             raise
 
-    def async_upload_handler(self, changed_fields):
-        sc = get_soundcloud_api()
-        filefield = get_soundfile_field(self.context)
-        accessors = get_soundcloud_accessors(self.context)
-        file_has_changed = changed_fields and filefield in changed_fields
-        mode = 'edit' if self.context.soundcloud_id else 'add'
-        # fetch blob
-        if self.context.soundcloud_id:
-            # we're are in soundcloud edit mode,
-            # iow this was already uploaded to soundcloud
-            tracks = sc.tracks(self.soundcloud_id)
-            if file_has_changed:
-                # delete what we have at soundcloud and create new entry
-                tracks(delete=True)
-                tracks = sc.tracks()
-        else:
-            # no soundcloud upload so far.
-            tracks = sc.tracks()
+    def _prepare_upload_data(self, filefield, changed_fields):
         track_data = {}
+        accessors = get_soundcloud_accessors(self.context)
         for iface, accessor in accessors:
             if changed_fields and accessor not in changed_fields:
                 continue
@@ -67,16 +51,44 @@ class SoundcloudUploaderView(BrowserView):
                 )
             elif not isinstance(track_data[accessor], basestring):
                 track_data[accessor] = str(track_data[accessor])
-        if not track_data:
-            return
-        track_data = self.upload(tracks, track_data)
-        setattr(self.context, 'trackdata', track_data)
-        setattr(self.context, 'soundcloud_id', track_data['id'])
-        self.context._p_changed = 1
+        return track_data
+
+    def _notify(self, mode):
         if mode == 'edit':
             notify(SoundcloudModifiedEvent(self.context))
         else:
             notify(SoundcloudCreatedEvent(self.context))
+
+    def _tracks(self, filefield, changed_fields, mode):
+        file_has_changed = changed_fields and filefield in changed_fields
+        sc = get_soundcloud_api()
+        if mode == 'edit':
+            # this was already uploaded to soundcloud
+            tracks = sc.tracks(self.soundcloud_id)
+            if file_has_changed:
+                # delete what we have at soundcloud and create new entry
+                tracks(delete=True)
+                # create a new one
+                tracks = sc.tracks()
+            return tracks
+        # no soundcloud upload so far, using this a new entry will be created
+        return sc.tracks()
+
+    def _save(self, track_data):
+        setattr(self.context, 'trackdata', track_data)
+        setattr(self.context, 'soundcloud_id', track_data['id'])
+        self.context._p_changed = 1
+
+    def async_upload_handler(self, changed_fields):
+        filefield = get_soundfile_field(self.context)
+        upload_data = self._prepare_upload_data(filefield, changed_fields)
+        if not upload_data:
+            return
+        mode = 'edit' if self.context.soundcloud_id else 'add'
+        sc_tracks = self._tracks(filefield, changed_fields, mode)
+        track_data = self.upload(sc_tracks, upload_data)
+        self._save(track_data)
+        self._notify(mode)
         return track_data
 
     def __call__(self):
