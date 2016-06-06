0c3cd4939dab376643db35ebba260c1375673c0c
Jens W. Klein <jk@kleinundpartner.at>
2016-06-06 16:06:42 +0200

    add upload works.

--- src/collective/soundcloud/taskqueue/uploader.py
+++ src/collective/soundcloud/taskqueue/uploader.py
@@ -3,7 +3,7 @@ from collective.soundcloud.directives import get_soundcloud_accessors
 from collective.soundcloud.directives import get_soundfile_field
 from collective.soundcloud.events import SoundcloudCreatedEvent
 from collective.soundcloud.events import SoundcloudModifiedEvent
-from collective.soundcloud.utils import get_soundcloud_api
+from collective.soundcloud.settings import get_soundcloud_settings
 from plone.protect.interfaces import IDisableCSRFProtection
 from Products.Five import BrowserView
 from StringIO import StringIO
@@ -11,6 +11,7 @@ from zope.event import notify
 from zope.interface import alsoProvides
 
 import logging
+import soundcloud
 
 
 logger = logging.getLogger(__name__)
@@ -18,19 +19,10 @@ logger = logging.getLogger(__name__)
 
 class SoundcloudUploaderView(BrowserView):
 
-    def upload(self, tracks, upload_track_data):
-        try:
-            return tracks(upload_track_data)
-        except Exception:
-            logger.exception('Can not modify at/upload to Soundcloud!')
-            raise
-
-    def _prepare_upload_data(self, filefield, asset_data_changed):
+    def _prepare_upload_data(self, filefield):
         track_data = {}
         accessors = get_soundcloud_accessors(self.context)
         for iface, accessor in accessors:
-            if accessor == 'asset_data' and not asset_data_changed:
-                continue
             adapter = iface(self.context)
             value = getattr(adapter, accessor)
             if value is None:
@@ -63,43 +55,39 @@ class SoundcloudUploaderView(BrowserView):
         else:
             notify(SoundcloudCreatedEvent(self.context))
 
-    def _tracks(self, filefield, asset_data_changed, mode):
-        sc = get_soundcloud_api()
-        if mode == 'edit':
-            # this was already uploaded to soundcloud
-            tracks = sc.tracks(self.context.soundcloud_id)
-            if asset_data_changed:
-                # delete what we have at soundcloud and create new entry
-                try:
-                    tracks(delete=True)
-                except:
-                    pass
-                # create a new one
-                tracks = sc.tracks()
-            return tracks
-        # no soundcloud upload so far, using this a new entry will be created
-        return sc.tracks()
-
     def _save(self, track_data):
         setattr(self.context, 'trackdata', track_data)
         setattr(self.context, 'soundcloud_id', track_data['id'])
         self.context._p_changed = 1
 
+    def _upload(self, client, upload_track_data, mode):
+        track = client.post(
+            '/tracks',
+            track=upload_track_data
+        )
+        return track
+
+    def _remove_if_exists(self, client, upload_track_data):
+        pass
+
     def __call__(self):
         self.request._sc_upload = True
         alsoProvides(self.request, IDisableCSRFProtection)
-
         self.request.response.setHeader('Content-Type', 'application/json')
-        asset_data_changed = self.request.form.get('asset_changed', 'False')
-        asset_data_changed = asset_data_changed == 'True'
 
         filefield = get_soundfile_field(self.context)
-        upload_data = self._prepare_upload_data(filefield, asset_data_changed)
+        upload_data = self._prepare_upload_data(filefield)
         if not upload_data:
             return
         mode = 'edit' if self.context.soundcloud_id else 'add'
-        sc_tracks = self._tracks(filefield, asset_data_changed, mode)
-        track_data = self.upload(sc_tracks, upload_data)
+        settings = get_soundcloud_settings()
+        client = soundcloud.Client(
+            client_id=settings.client_id,
+            access_token=settings.token
+        )
+        if mode == 'edit':
+            self._remove_if_exists(client, upload_data)
+        track_data = self._upload(client, upload_data, mode)
         self._save(track_data)
         self._notify(mode)
         return ''
